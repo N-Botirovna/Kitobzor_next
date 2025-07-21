c75693f3d63fd16adb5d9609a0485fcbf0b7deb6
User <nargizatosheva20@gmail.com>
2025-07-21 18:03:46 +0500

    commit

--- src/components/ClientLayout.tsx
+++ src/components/ClientLayout.tsx
@@ -1,38 +1,49 @@
-// components/ClientLayout.tsx
 "use client"
 
-import { usePathname } from "../../node_modules/next/navigation"
+import { useEffect, useState } from "react"
+import { usePathname, useRouter } from "next/navigation"
 import SiteFooter from "./footer/SiteFooter"
 import TagHeader from "./header/HeaderCategories"
 import NavbarHeader from "./header/NavbarHeader"
 import TopHeader from "./header/TopHeader"
 import { ThemeProvider } from "./theme-provider"
 
-
-
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname()
+  const router = useRouter()
   const hideLayoutFor = ["/login", "/404", "/profile"]
   const hideLayout = hideLayoutFor.includes(pathname)
+  const [isLoading, setIsLoading] = useState(true)
 
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+    console.log("Token mavjud:", !!token)
+
+    if (!token && pathname !== "/login") {
+      router.push("/login")
+    } else {
+      setIsLoading(false)
+    }
+  }, [pathname, router])
+
+  if (isLoading) return null
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       {!hideLayout ? (
         <>
-        <div className="h-[33vh]">
-          <div className="container mx-auto">
-            <TopHeader />
+          <div className="h-[33vh]">
+            <div className="container mx-auto">
+              <TopHeader />
+            </div>
+            <NavbarHeader />
+            <TagHeader />
           </div>
-          <NavbarHeader />
-          <TagHeader />
-        </div>
-        <main className="min-h-screen">{children}</main>
-        <SiteFooter />
-      </>
+          <main className="min-h-screen">{children}</main>
+          <SiteFooter />
+        </>
       ) : (
         children
       )}
     </ThemeProvider>
   )
 }
-
